387351c2ce941fbc5e579d5186109135604f4d85
adamw <wlosikk.adam@gmail.com>
2024-02-14 16:48:57 +0100

    [Feature] Commit ReGex work in progress

--- python_zaawansowany/main.py
+++ python_zaawansowany/main.py
@@ -1,21 +1,27 @@
-from python_zaawansowany.zadania.regex import Task1Regex
+from python_zaawansowany.zadania.regex import Task1Regex, Task2Regex, Task3Regex
 
 
 def main():
     while True:
         try:
-            traning = int(input("\nAby zakończyć wybierz 0 lub wprowadź numer szkolenia: "))
-            if traning == 0:
+            training = int(input("\nAby zakończyć wybierz 0 lub wprowadź numer szkolenia: "))
+            if training == 0:
                 break
             task = int(input("Wprowadź numer zadania: "))
         except ValueError:
             print("Prosze nie psuć!")
             continue
 
-        if traning == 1:
+        if training == 1:
             if task == 1:
-                task1 = Task1Regex(traning, task)
+                task1 = Task1Regex(training, task)
                 task1.rozwiazanie()
+            elif task == 2:
+                task2 = Task2Regex(training, task)
+                task2.rozwiazanie()
+            elif task == 3:
+                task3 = Task3Regex(training, task)
+                task3.rozwiazanie()
 
 
 if __name__ == "__main__":
--- python_zaawansowany/zadania/regex.py
+++ python_zaawansowany/zadania/regex.py
@@ -9,17 +9,74 @@ class Task1Regex(Szkolenie):
         Napisz program, który sprawdzi, czy string zawiera tylko
         i wyłącznie zbiór następujących znaków: (a-z, A-Z i 0-9).
         """
+
+    def __init__(self, training, task):
+        super().__init__(training, task)
+
+    @print_doc
+    def rozwiazanie(self):
+        text = "Nie tylko odpowiednie znaki!!"
+        text2 = "Tylkoodpowiednieznaki234"
+        print(self.check(text))
+        print(self.check_compile(text))
+        print(self.check(text2))
+        print(self.check_compile(text2))
+
+    @staticmethod
+    def check_compile(text):
+        """Metoda kompilująca wzorzec, na podstawie którego szukamy dopasowań"""
+        pattern = re.compile(r'^[a-zA-Z0-9]+$')
+        return bool(pattern.match(text))
+
+    @staticmethod
+    def check(text):
+        """Metoda szukająca dopasowań """
+        match = re.match(r'^[a-zA-Z0-9]+$', text)
+        return bool(match)
+
+
+class Task2Regex(Szkolenie):
+    """Zad. 2
+        Sprawdź, czy string rozpoczyna się pojedynczą cyfrą: 0 lub literą ‘b’.
+        """
+
     def __init__(self, szkolenie, zadanie):
         super().__init__(szkolenie, zadanie)
 
     @print_doc
     def rozwiazanie(self):
-        text = ("Nie tylko odpowiednie zanki !!")
-        text2 = "Tylkoodpowiednieznaki234"
+        text = "Nie tylko odpowiednie znaki !!"
+        text1 = "0Tylkoodpowiednieznaki234"
+        text2 = "bTylkoodpowiednieznaki234"
         print(self.check(text))
+        print(self.check(text1))
         print(self.check(text2))
 
-    def check(self, text):
-        """Metoda """
-        pattern = re.compile(r'^[a-zA-Z0-9]+$')
-        return bool(pattern.match(text))
+    @staticmethod
+    def check(text):
+        """Metoda sprawdzająca początek str"""
+        return bool(re.match(r'^[0b]', text))
+
+
+class Task3Regex(Szkolenie):
+    """Zad. 3
+        Sprawdzaj, czy podany string zawiera ciąg dowolnych małych liter rozdzielonych znakiem _, np. aab_cbbbc
+        """
+
+    def __init__(self, training, task):
+        super().__init__(training, task)
+
+    @print_doc
+    def rozwiazanie(self):
+        text = "nie tylko odpowiednie znaki !!"
+        text1 = "aab_cbbbc"
+        text2 = "bylkoodpow_iednieznaki"
+        print(self.check(text))
+        print(self.check(text1))
+        print(self.check(text2))
+
+    @staticmethod
+    def check(text):
+        pattern = r'^[a-z]+_+[a-z]+$'
+        return bool(re.match(pattern, text))
+
