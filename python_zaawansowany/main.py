2f6468324980828de72bffd477f79d6464c7c24a
AdamWlosik <wlosikk.adam@gmail.com>
2024-02-14 14:14:06 +0100

    [feature] regex work in progress

--- python_zaawansowany/main.py
+++ python_zaawansowany/main.py
@@ -1,17 +1,21 @@
+from python_zaawansowany.zadania.regex import Task1Regex
+
+
 def main():
     while True:
         try:
-            szkolenie = int(input("\nAby zakończyć wybierz 0 lub wprowadź numer szkolenia: "))
-            if szkolenie == 0:
+            traning = int(input("\nAby zakończyć wybierz 0 lub wprowadź numer szkolenia: "))
+            if traning == 0:
                 break
-            zadanie = int(input("Wprowadź numer zadania: "))
+            task = int(input("Wprowadź numer zadania: "))
         except ValueError:
             print("Prosze nie psuć!")
             continue
 
-        if szkolenie == 1:
-            if zadanie == 1:
-                pass
+        if traning == 1:
+            if task == 1:
+                task1 = Task1Regex(traning, task)
+                task1.rozwiazanie()
 
 
 if __name__ == "__main__":
--- python_zaawansowany/zadania/regex.py
+++ python_zaawansowany/zadania/regex.py
@@ -1,11 +1,25 @@
+import re
+
 from Szkolenie import Szkolenie
 from helpers import print_doc
 
 
-class Zadanie1(Szkolenie):
+class Task1Regex(Szkolenie):
+    """Zad. 1
+        Napisz program, który sprawdzi, czy string zawiera tylko
+        i wyłącznie zbiór następujących znaków: (a-z, A-Z i 0-9).
+        """
     def __init__(self, szkolenie, zadanie):
         super().__init__(szkolenie, zadanie)
 
     @print_doc
     def rozwiazanie(self):
-        pass
+        text = ("Nie tylko odpowiednie zanki !!")
+        text2 = "Tylkoodpowiednieznaki234"
+        print(self.check(text))
+        print(self.check(text2))
+
+    def check(self, text):
+        """Metoda """
+        pattern = re.compile(r'^[a-zA-Z0-9]+$')
+        return bool(pattern.match(text))
