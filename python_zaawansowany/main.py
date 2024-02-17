c286d704afe0064c06e4fc38ccbf74d4f595f71e
adamw <wlosikk.adam@gmail.com>
2024-02-17 17:54:51 +0100

    [Feature] Dekoratory done

--- python_zaawansowany/main.py
+++ python_zaawansowany/main.py
@@ -1,4 +1,4 @@
-from python_zaawansowany.zadania.dekoratory import Task1Dek
+from python_zaawansowany.zadania.dekoratory import Task1Dek, Task2Dek, Task3Dek, Task4Dek, Task5Dek, Task6Dek
 
 
 def main():
@@ -18,6 +18,21 @@ def main():
             if task == 1:
                 task1 = Task1Dek(training, task)
                 task1.solution()
+            elif task == 2:
+                task2 = Task2Dek(training, task)
+                task2.solution()
+            elif task == 3:
+                task3 = Task3Dek(training, task)
+                task3.solution()
+            elif task == 4:
+                task4 = Task4Dek(training, task)
+                task4.solution()
+            elif task == 5:
+                task5 = Task5Dek(training, task)
+                task5.solution()
+            elif task == 6:
+                task6 = Task6Dek(training, task)
+                task6.solution()
 
 
 if __name__ == "__main__":
