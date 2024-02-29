846ef56b0c5a9126fd8d6e8840dd68a13a929916
adamw <wlosikk.adam@gmail.com>
2024-02-29 19:35:39 +0100

    [Feature] Commit Exceptions work in progress

--- python_zaawansowany/main.py
+++ python_zaawansowany/main.py
@@ -6,7 +6,7 @@ from python_zaawansowany.zadania.dekoratory import (
     Task5Dek,
     Task6Dek,
 )
-from python_zaawansowany.zadania.exceptions import FileHandlerEX
+from python_zaawansowany.zadania.exceptions import FileHandlerEX, Task2EX
 from python_zaawansowany.zadania.programowanie_funkcyjne import (
     Task1PF,
     Task2PF,
@@ -125,9 +125,10 @@ def main():
                 task8.solution()
         elif training == 5:
             if task == 1:
-                task1 = FileHandlerEX(
-                    training, task,
-                    "a", 12, 2000)
+                task1 = FileHandlerEX(training, task, "a", 12, 2000)
+            elif task == 2:
+                task2 = Task2EX(training, task)
+                task2.solution()
 
 
 if __name__ == "__main__":
--- python_zaawansowany/zadania/exceptions.py
+++ python_zaawansowany/zadania/exceptions.py
@@ -1,25 +1,28 @@
+from helpers import print_doc
 from python_zaawansowany.training import Training
 
 
 class FileHandlerEX(Training):
     """Zad. 1
-        Przygotuj klasę FileHandler, która symulować będzie funkcjonalność obsługiwania plików w Javie.
-        Ma ona przechowywać pola takie jak: file_path, no_connectors, max_file_size.
-        Będa one ustawiane z poziomu odpowiednich setterów jak i konstruktora.
-        Klasa ta ma dodatkowo przechowywać takie metody jak read_content, save_to_file,
-        w których umieścisz, tzw. dummy printy (będziesz drukował dowolny tekst).
+    Przygotuj klasę FileHandler, która symulować będzie funkcjonalność obsługiwania plików w Javie.
+    Ma ona przechowywać pola takie jak: file_path, no_connectors, max_file_size.
+    Będa one ustawiane z poziomu odpowiednich setterów jak i konstruktora.
+    Klasa ta ma dodatkowo przechowywać takie metody jak read_content, save_to_file,
+    w których umieścisz, tzw. dummy printy (będziesz drukował dowolny tekst).
 
-        Celem tego zadania nie jest stworzenie odpowiedniej logiki biznesowej dla klasy,
-        tylko zaprojektowanie klasy, która będzie zwracała odpowiedni user-defined wyjątek
-        w zależności od przekazania do obiektu klasy niewłaściwej danej, np. pustego stringa,
-        który będzie miał być umieszczony pod polem file_path.
+    Celem tego zadania nie jest stworzenie odpowiedniej logiki biznesowej dla klasy,
+    tylko zaprojektowanie klasy, która będzie zwracała odpowiedni user-defined wyjątek
+    w zależności od przekazania do obiektu klasy niewłaściwej danej, np. pustego stringa,
+    który będzie miał być umieszczony pod polem file_path.
 
-        Dodatkowe warunki:
-        Wartość noConnector nie może przekroczyć wartości 10
-        maxFileSize musi być zawsze liczbą czterocyfrową
-        """
+    Dodatkowe warunki:
+    Wartość noConnector nie może przekroczyć wartości 10
+    maxFileSize musi być zawsze liczbą czterocyfrową
+    """
 
-    def __init__(self, training: int, task: int, file_path, no_connectors, max_file_size):
+    def __init__(
+        self, training: int, task: int, file_path, no_connectors, max_file_size
+    ):
         super().__init__(training, task)
         self.max_file_size = max_file_size
         self.no_connectors = no_connectors
@@ -67,3 +70,56 @@ class FileHandlerEX(Training):
 
     def save_to_file(self):
         pass
+
+
+class DivisionByZeroError(Exception):
+
+    def __str__(self):
+        return "Dzielenie przez 0"
+
+
+class Task2EX(Training):
+    def __init__(self, training: int, task: int):
+        super().__init__(training, task)
+
+    @print_doc
+    def solution(self):
+        self.main()
+
+    @staticmethod
+    def example1():
+        for i in range(3):
+            try:
+                x = int(input("enter a number: "))
+                y = int(input("enter another number: "))
+                if y == 0:
+                    raise DivisionByZeroError
+            except ValueError:
+                print("Nie podano wartości int")
+            print(x, "/", y, "=", x / y)
+
+    @staticmethod
+    def example2(input_list):
+        print("\n\nExample 2")
+        sum_of_pairs: input_list = []
+        for i in range(len(input_list)):
+            sum_of_pairs.append(input_list[i] + input_list[i + 1])
+
+        print("sum_of_pairs = ", sum_of_pairs)
+
+    @staticmethod
+    def printUpperFile(file_name):
+        file = open(file_name, "r")
+        for line in file:
+            print(line.upper())
+        file.close()
+
+    def main(self):
+        self.example1()
+        l: list = [10, 3, 5, 6, 9, 3]
+        self.example2(l)
+        self.example2([10, 3, 5, 6, "NA", 3])
+        self.example3([10, 3, 5, 6])
+
+        self.printUpperFile("doesNotExistYest.txt")
+        self.printUpperFile("./Dessssktop/misspelled.txt")
