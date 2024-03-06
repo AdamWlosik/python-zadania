c87a48e7455bb80057faa3865567048a244f7307
adamw <wlosikk.adam@gmail.com>
2024-03-06 17:21:25 +0100

    commit

--- ciphering_rot47_rot13/tests/test_main.py
+++ ciphering_rot47_rot13/tests/test_main.py
@@ -8,12 +8,14 @@ from ciphering_rot47_rot13.functionality.main import MenuApp
 
 class TestMenuApp:
     # TODO
-    # nie działa test_button mimo ustawienia fixture button ma dalej wartość none
+    # nie działa test_button mimo ustawienia
+    # fixture button ma dalej wartość none
 
     @pytest.fixture
     def set_app_mock(self, mocker: MockerFixture):
         tkinter_root = tkinter.Tk()
         main_app = MenuApp(tkinter_root)
+        assert main_app.encrypt_button is not None
         return main_app
 
     def test_title(self, set_app_mock):
